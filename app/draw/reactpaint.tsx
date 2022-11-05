ef793f6b3e7112aed6c3a4a62d5cbe75924b95d0
Sanskar Omar <heysanskar0.0@gmail.com>
2022-11-06 02:32:21 +0530

    Redesigned UI, Improved UX

--- app/draw/reactpaint.tsx
+++ app/draw/reactpaint.tsx
@@ -1,61 +1,55 @@
 "use client"
 
-import React, { MouseEvent, useState } from "react"
+import React, { useState } from "react";
 
-import MenuBar from "./menubar"
-import Content from "./content"
-import ColorPanel from "./colorpanel"
+import MenuBar from "./menubar";
+import Content from "./content";
 
-const defaultColor = "black"
-const defaultTool = "Pencil"
+const defaultTool = "Pencil";
 
-import pencil from "./images/pencil.svg"
-import line from "./images/line.svg"
-import brush from "./images/brush.svg"
-import fill from "./images/fill.svg"
-import rectangle from "./images/rectangle.svg"
-import text from "./images/text.svg"
-import circle from "./images/circle.svg"
-import erase from "./images/erase.svg"
-import picker from "./images/picker.svg"
+import pencil from "./images/pencil.svg";
+import line from "./images/line.svg";
+import brush from "./images/brush.svg";
+import fill from "./images/fill.svg";
+import erase from "./images/erase.svg";
+import rectangle from "./images/rectangle.svg";
+import circle from "./images/circle.svg";
+import text from "./images/text.svg";
+import picker from "./images/picker.svg";
 
 const defaultToolbarItems = [
-	{ name: "Pencil", image: pencil },
-	{ name: "Line", image: line },
-	{ name: "Brush", image: brush },
-	{ name: "Fill", image: fill },
-	{ name: "Text", image: text },
-	{ name: "Rectangle", image: rectangle },
-	{ name: "Circle", image: circle },
-	{ name: "Erase", image: erase },
-	{ name: "Picker", image: picker },
-]
+  { name: "Pencil", image: pencil },
+  { name: "Line", image: line },
+  { name: "Brush", image: brush },
+  { name: "Erase", image: erase },
+  { name: "Fill", image: fill },
+  { name: "Rectangle", image: rectangle },
+  { name: "Text", image: text },
+  { name: "Circle", image: circle },
+  { name: "Picker", image: picker },
+];
 
 const ReactPaint = () => {
-	const [color, setColor] = useState(defaultColor)
-	const [selectedItem, setSelectedItem] = useState(defaultTool)
-	const [toolbarItems, setToolbarItems] = useState(defaultToolbarItems)
+  const [color, setColor] = useState("black");
+  const [selectedItem, setSelectedItem] = useState(defaultTool);
+  const [toolbarItems, setToolbarItems] = useState(defaultToolbarItems);
 
-	const changeColor = (e: MouseEvent<HTMLDivElement>) => {
-		setColor((e.target as HTMLDivElement).style.backgroundColor)
-	}
+  const changeTool = (_event: any, tool: any) => {
+    setSelectedItem(tool);
+  };
 
-	const changeTool = (_event: any, tool: any) => {
-		setSelectedItem(tool)
-	}
-
-	return (
-		<>
-			<MenuBar />
-			<Content
-				items={toolbarItems}
-				activeItem={selectedItem}
-				handleClick={changeTool}
-				color={color}
-			/>
-			<ColorPanel selectedColor={color} handleClick={changeColor} />
-		</>
-	)
-}
+  return (
+    <>
+      <MenuBar />
+      <Content
+        items={toolbarItems}
+        activeItem={selectedItem}
+        handleClick={changeTool}
+        color={color}
+        setColor={setColor}
+      />
+    </>
+  );
+};
 
 export default ReactPaint
