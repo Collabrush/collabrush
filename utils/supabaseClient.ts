5aa3d778a8e526bd887218d478aeb35cda375472
Sanskar Omar <heysanskar0.0@gmail.com>
2022-11-12 03:37:57 +0530

    Dashboard Changed

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,10 +1,6 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-// console.log(supabaseUrl, supabaseKey)
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // "https://<project-id>.supabase.co"
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // "<anon-key>"
+const supabase = createClient(supabaseUrl, supabaseKey); // create a supabase client
+export default supabase;
--- utils/useIntervalhook.ts
+++ utils/useIntervalhook.ts
@@ -16,11 +16,9 @@ function useInterval(callback: () => void, delay: number | null) {
 		// Don't schedule if no delay is specified.
 		// Note: 0 is a valid value for delay.
 		if (!delay && delay !== 0) {
-			return
-		}
-
-		const id = setInterval(() => savedCallback.current(), delay)
-
+      return;
+    }
+    const id = setInterval(() => savedCallback.current(), delay);
 		return () => clearInterval(id)
 	}, [delay])
 }
